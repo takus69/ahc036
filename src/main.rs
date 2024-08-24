a40dd4d27ea0fc9b4b4a202cb52c1f53af03294c
Takuya Saito <takus4649@gmail.com>
2024-08-24 13:11:26 +0900

    v2: 信号の制御でbを最大限使用する(スコア: 334,740, 予測: 349,674)

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ impl Solver {
                 a[i] = i;
             }
         }
-        let b = vec![usize::MAX; n];
+        let b = vec![usize::MAX; lb];
         let ans: Vec<String> = Vec::new();
         let score = 0;
         let lt = 0;
@@ -68,6 +68,7 @@ impl Solver {
             next = prev[next];
             path.push(next);
         }
+        path.pop();
         path.reverse();
 
         path
@@ -75,18 +76,53 @@ impl Solver {
 
     fn solve(&mut self) {
         let mut from = 0;
-        for to in self.t_list.iter() {
+        let t_list = self.t_list.clone();
+        for to in t_list.iter() {
             println!("# from: {}, to: {}", from, to);
             let path = self.bfs(from, *to);
             self.lt += path.len()-1;
-            for p in path[1..].iter() {
-                self.ans.push(format!("s {} {} {}", 1, p, 0));
-                self.score += 1;
-                self.ans.push(format!("m {}", p));
+            for (i, p) in path.iter().enumerate() {
+                let target = path[i..path.len().min(i+self.lb)].to_vec();
+                if !self.can_move(p) {
+                    self.switch(target);
+                }
+                self.r#move(p);
             }
             from = *to;
         }
 
+    }
+
+    fn switch(&mut self, target: Vec<usize>) {
+        let mut min_i = usize::MAX;
+        let mut max_i = 0;
+        for t in target.iter() {
+            let index = self.a.iter().position(|&x| x == *t).unwrap();
+            let dist = max_i.max(index) - min_i.min(index) + 1;
+            if dist > self.lb {
+                break;
+            } else {
+                min_i = min_i.min(index);
+                max_i = max_i.max(index);
+            }
+        }
+        let l = max_i - min_i + 1;
+        let s_a = min_i;
+        let s_b = 0;
+        println!("# s {} {} {}", l, s_a, s_b);
+        self.b[s_b..(s_b+l)].clone_from_slice(&self.a[s_a..(s_a+l)]); 
+        self.ans.push(format!("s {} {} {}", l, s_a, s_b));
+        self.score += 1;
+
+    }
+
+    fn can_move(&self, p: &usize) -> bool {
+        self.b.contains(p)
+    }
+    
+    fn r#move(&mut self, p: &usize) {
+        println!("# m {}", p);
+        self.ans.push(format!("m {}", p));
     }
     
     fn ans(self) {
@@ -120,8 +156,7 @@ fn main() {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_solver_new() {
+    fn setup() -> Solver {
         let n = 7;
         let m = 9;
         let t = 3;
@@ -136,13 +171,14 @@ mod tests {
             (100, 0), (200, 0), (200, 100), (100, 100),
             (0, 200), (0, 100), (0, 0),
         ];
-        let solver = Solver::new(n, m, t, la, lb, uv, t_list, xy);
-        assert_eq!(solver.g[&0], [1, 2, 3, 6]);
-        assert_eq!(solver.g[&1], [0, 2]);
-        assert_eq!(solver.g[&2], [0, 1, 3]);
-        assert_eq!(solver.g[&3], [0, 2, 4]);
-        assert_eq!(solver.g[&4], [3, 5]);
-        assert_eq!(solver.g[&5], [4, 6]);
-        assert_eq!(solver.g[&6], [5, 0]);
+        Solver::new(n, m, t, la, lb, uv, t_list, xy)
+    }
+    
+    #[test]
+    fn test_switch() {
+        let mut solver = setup();
+        assert_eq!(solver.a, [0, 1, 2, 3, 4, 5, 6]);
+        solver.switch(vec![3, 0, 2, 4]);
+        assert_eq!(solver.b, [0, 1, 2, 3]);
     }
 }
